ff1946594e9e8a20eb7a59bf70dc323edc21aeaa
NotTrashraD <bellie1579@gmail.com>
2025-05-08 02:52:58 +0200

    push

--- lua/arc9/common/attachments_bulk/wz_sh_optics.lua
+++ lua/arc9/common/attachments_bulk/wz_sh_optics.lua
@@ -87,6 +87,62 @@ ATT.ModelOffset = Vector(0, 0, 0)
 ARC9.LoadAttachment(ATT, "wz_opt_3rfl")
 
 
+ATT = {}
+
+
+ATT.Free = true
+
+
+ATT.PrintName = "3X RFL-OPTIC"
+ATT.CompactName = "3RFL"
+ATT.Description = [[An infrared 3-8x rifle optic of Russian origins.]]
+
+ATT.Folder = "Thermals"
+ATT.SpeedMultSights = 0.8
+ATT.SprintToFireTimeAdd = 0.5
+ATT.SpeedMult = 0.90
+
+ATT.CustomPros = {
+    ["Magnification"] = "3-8x",
+    ["Thermal Capabilities"] = ""
+}
+
+
+
+ATT.Model = "models/weapons/r_mwii/att/opt/att_r_mwii_opt_3rfl.mdl"
+
+ATT.Sights = {
+    {
+        Pos = Vector(0, 10, -1.82),
+        Ang = Angle(0, -0, 0),
+        Magnification = 1.15,
+        ViewModelFOV = 60,
+    }
+}
+
+
+ATT.RTScopeAdjustable = true -- adjustable scope settings
+ATT.RTScopeAdjustmentLevels = 2
+ATT.RTScopeFOVMin = 3
+ATT.RTScopeFOVMax = 6
+
+
+ATT.RTScope = true
+
+
+
+
+ATT.RTScopeReticle = Material("vgui/rets/thermal_1.png", "mips smooth")
+ATT.RTScopeReticleScale = 1.15
+ATT.Category = {"wz2_sh_optics"}
+ATT.MenuCategory = "ARC9 - Warzone 2.0"
+
+ATT.ModelOffset = Vector(0, 0, 0)
+
+
+ARC9.LoadAttachment(ATT, "wz_opt_6xshortzoom")
+
+
 
 ATT = {}
 
